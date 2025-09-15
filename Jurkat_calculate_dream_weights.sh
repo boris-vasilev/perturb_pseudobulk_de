994c8fa8117e5dfb7fa073ea52a8387262b70b8a
Boris Vasilev <borisvasilev395@gmail.com>
2025-09-15 14:41:43 +0100

    Calculate dream weights separately and store rds

---
+++ Jurkat_calculate_dream_weights.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH -A MRC-BSU-SL2-CPU
+#SBATCH -p icelake-himem
+#SBATCH -N 1
+#SBATCH --exclusive
+#SBATCH -t 10:00:00
+
+#. /etc/profile.d/modules.sh
+
+#module load miniconda/3
+source ~/.bashrc
+conda activate env_nf
+
+
+Rscript calculate_dream_weights.R
