b2c4f5734fc8eec7cacf032176d54d2e37aca8a0
Boris Vasilev <borisvasilev395@gmail.com>
2025-09-23 13:26:59 +0100

    Add K562_essential

--- K562_essential.sh
+++ K562_essential.sh
@@ -1,21 +1,13 @@
 #!/bin/bash
-#SBATCH -A MRC-BSU-SL2-CPU
-#SBATCH -p icelake-himem
-#SBATCH -N 1
-#SBATCH --exclusive
-#SBATCH -t 4:00:00
-
-#. /etc/profile.d/modules.sh
-
-#module load miniconda/3
 . /etc/profile.d/modules.sh
 module load gcc/11
 
+#module load miniconda/3
 source ~/.bashrc
 conda activate env_nf
 
-nextflow main.nf \
-    --inputSeuratObject /home/biv22/rds/rds-mrc-bsu-csoP2nj6Y6Y/biv22/AMD/data/perturb/seurat/K562_essential_raw_singlecell_01.h5ad \
-    --outputDir /home/biv22/rds/rds-mrc-bsu-csoP2nj6Y6Y/biv22/AMD/data/perturb/DEGs/K562_pseudo \
-    --numWorkers 75 
+nextflow main.nf -resume \
+    --inputSeuratObject /rds/project/rds-csoP2nj6Y6Y/biv22/perturb_pseudobulk_de/data/K562_essential_raw_singlecell_01.h5ad \
+    --outputDir /home/biv22/rds/rds-mrc-bsu-csoP2nj6Y6Y/biv22/data/perturb/K562_essential \
+    --numWorkers 100
 
